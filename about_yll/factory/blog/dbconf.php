53d97466147c242feda09b5284efdff9f4d21e01
yllagimi <yll.agimi@gmail.com>
2024-01-10 14:37:33 -0500

    removing private

--- about_yll/factory/blog/dbconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $driver = 'mysql:host=localhost;foody';
-    $user = 'weby';
-    $pass = 'testing';
-    $attr = array(PDO::ATTR_PERSISTENT => true, 
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-?>
